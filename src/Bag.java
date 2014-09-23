4bdcf57d3749ee69223b4a54334bf3c502985cde
Carla Suarez <csuarez@thoughtworks.com>
2014-09-23 10:58:25 -0500

    Adding existing project to git

---
+++ src/Bag.java
@@ -0,0 +1,22 @@
+public class Bag {
+
+    private int[] items;
+    private int count;
+
+    public Bag() {
+        items = new int[1];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public void add(final int item) {
+        items[count] = item;
+        count++;
+    }
+
+    public int size() {
+        return count;
+    }
+}
---
+++ test/TestBag.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestBag {
+
+    @Test
+    public void shouldStartEmpty() {
+        final Bag bag = new Bag();
+        final boolean empty = bag.isEmpty();
+        assertTrue(empty);
+    }
+
+    @Test
+    public void shouldNotBeEmptyAfterAddingItem() {
+        final Bag bag = new Bag();
+        bag.add(1);
+        final boolean empty = bag.isEmpty();
+        assertFalse(empty);
+    }
+
+    @Test
+    public void shouldStartSizeOnZero() {
+        final Bag bag = new Bag();
+        int size = bag.size();
+        assertEquals(0, size);
+    }
+
+    @Test
+    public void shouldIncrementCounterByOne() {
+        final Bag bag = new Bag();
+        bag.add(1);
+        final int size = bag.size();
+        assertEquals(1, size);
+    }
+
+    @Test
+    public void shouldIncrementCounterByOnePerItemAdded() {
+        final Bag bag = new Bag();
+        bag.add(1);
+        bag.add(1);
+        final int size = bag.size();
+        assertEquals(2, size);
+    }
+}
