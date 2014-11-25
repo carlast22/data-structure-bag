5fcdef57a20d1d03ecb71128eb0edce16fcc2f76
Carla Suarez <csuarez@thoughtworks.com>
2014-11-25 18:50:41 -0500

    Add pop to delete first element in queue

--- src/main/java/com/thoughtworks/queue/Queue.java
+++ src/main/java/com/thoughtworks/queue/Queue.java
@@ -3,6 +3,7 @@ package com.thoughtworks.queue;
 import java.lang.reflect.Array;
 
 public class Queue<T> {
+
     private boolean empty;
     private Class<T> type;
     private T[] items;
@@ -20,7 +21,7 @@ public class Queue<T> {
     public void add(final T item) {
         final int capacity = items.length;
         final T[] aux = createArrWithCapacityIncreasedByOne(capacity);
-        copyElements(items, aux);
+        copyAllElements(items, aux);
         aux[count] = item;
         items = aux;
         count++;
@@ -30,15 +31,38 @@ public class Queue<T> {
         return count;
     }
 
+    public T pop() {
+        final T first = items[0];
+        final int capacity = items.length;
+        final T[] aux = createArrWithCapacityDecreasedByOne(capacity);
+        copyAllElementsButFirst(items, aux);
+        items = aux;
+        count --;
+        return first;
+    }
+
     private T[] createArrWithCapacityIncreasedByOne(final int capacity) {
         final int newCapacity = capacity + 1;
         final T[] tsArr = (T[]) Array.newInstance(type, newCapacity);
         return tsArr;
     }
 
-    private void copyElements(final T[] source, final T[] target) {
-        for(int i =0; i<source.length; i++) {
+    private T[] createArrWithCapacityDecreasedByOne(final int capacity) {
+        final int newCapacity = capacity - 1;
+        final T[] tsArr = (T[]) Array.newInstance(type, newCapacity);
+        return tsArr;
+    }
+
+    private void copyAllElements(final T[] source, final T[] target) {
+        for (int i = 0; i < source.length; i++) {
             target[i] = source[i];
         }
     }
+
+    private void copyAllElementsButFirst(final T[] source, final T[] target) {
+        for (int i = 0; i < target.length; i++) {
+            target[i] = source[i + 1];
+        }
+    }
 }
+
--- src/test/java/com/thoughtworks/queue/QueueTest.java
+++ src/test/java/com/thoughtworks/queue/QueueTest.java
@@ -2,7 +2,9 @@ package com.thoughtworks.queue;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class QueueTest {
@@ -20,5 +22,23 @@ public class QueueTest {
         queue.add(1);
         final boolean empty = queue.isEmpty();
         assertFalse(empty);
+    }
+
+    @Test
+    public void shouldAddAnotherItem() {
+        final Queue<Integer> queue = new Queue<Integer>(Integer.class);
+        queue.add(1);
+        queue.add(2);
+        final int lenght = queue.size();
+        assertThat(lenght, is(2));
+    }
+
+    @Test
+    public void shouldRemoveTheItemInTheQueue() {
+        final Queue<Integer> queue = new Queue<Integer>(Integer.class);
+        queue.add(1);
+        queue.pop();
+        final int lenght = queue.size();
+        assertThat(lenght, is(0));
     }
 }
