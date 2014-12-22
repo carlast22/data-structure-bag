f0d22e6bcf0ba0674fdc559204a409db27be8f18
Carla Suarez <csuarez@thoughtworks.com>
2014-12-22 13:13:51 -0500

    Extract duplicate code in one method

--- src/main/java/com/thoughtworks/estructures/Stack.java
+++ src/main/java/com/thoughtworks/estructures/Stack.java
@@ -33,16 +33,11 @@ public class Stack<T> {
     public T pop() {
         final T last = items[items.length - 1];
         final int capacity = items.length;
-        final T[] aux = createArrWithCapacityDecreasedByOne(capacity - 1);
+        final T[] aux = createArrWithCapacityIncreasedByOne(capacity - 1);
         copyAllElementsButLast(items, aux);
         items = aux;
         count --;
         return last;
-    }
-
-    private T[] createArrWithCapacityDecreasedByOne(final int capacity) {
-        final T[] tsArr = (T[]) Array.newInstance(type, capacity);
-        return tsArr;
     }
 
     private T[] createArrWithCapacityIncreasedByOne(final int capacity) {
